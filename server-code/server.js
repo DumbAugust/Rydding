b1bf2152399633a5d244f2c9c139e511802f352c
DumbAugust <carlaugust.sponberg@gmail.com>
2024-02-26 21:40:37 +0100

    sql setup

--- server-code/server.js
+++ server-code/server.js
@@ -1,12 +1,17 @@
 const express = require('express')
+const path = require('path')
+const sq = require('better-sqlite3')
 
+const publicPath = path.join(__dirname, '../public')
+const db = sq('database.db', {verbose: console.log})
 const app = express()
 
-function(request, response) {
-    response.send("Hello world")
-}
+app.use(express.static(publicPath))
 
-app.get('', rootRoute)
+
+app.get('/about', (req, res) => {
+    res.send("Welcome to my application")
+})
 
 app.listen(3000, () => {
     console.log('server is up on port 3000')
