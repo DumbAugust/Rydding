da59f3b39bef6daae9d004ea7fe295914f73942c
DumbAugust <carlaugust.sponberg@gmail.com>
2024-03-02 20:41:06 +0100

    async funciton layout and get function that sends data to a adress

--- server-code/server.js
+++ server-code/server.js
@@ -8,18 +8,23 @@ const db = sq('databaseTest.db', {verbose: console.log})
 const app = express()
 
 // global data variables
+
 data = []
+
+
+// function which gathers data from the database
 
 function gatherData(data, name, pass) {
     data = []
-    data.push(db.prepare(`select * from profile where name = "${name}" and password = "${pass}"`))
-    data.push(db.prepare(`select * from family where ID = ${data[0].ID}`))
-    data.push(db.prepare(`select * from tasks where familyID = ${data[1].ID}`))
+    data.push(db.prepare(`select * from profile where name = ? and password = ?`).get(name, pass))
+    data.push(db.prepare(`select * from family where codes = ?`).get(12345))
+    data.push(db.prepare("select * from profileFamily where familyID = ?").all(3))
+    data.push(db.prepare(`select * from familyTasks where familyID = ?`).all(3))
     console.log(data)
 }
 
-gatherData(data, 'Carl', 'banan123' )
 
+// i dont really know what this is yet
 app.use(express.static(publicPath))
 app.use(session({
     secret: 'still a secret',
@@ -27,9 +32,31 @@ app.use(session({
     saveUninitialized: false,
 }));
 
+// get funksjon som sender deg til html siden når nettsiden åpnes
+
 app.get('/', (req, res) => {
     res.redirect("/main.html")
 });
+
+// get function som samler data og sender det til adressen til /data
+
+app.get('/data', (req, res) => {
+    gatherData(data, 'Carl', 'banan123' )
+    res.send(data)
+})
+
+/*
+
+Mal for async function som skal oppdateres
+
+async function updateData() {
+    let response =  await fetch('/data');
+    let data = await response.json();
+
+    noe kode som oppdaterer siden
+}
+
+*/
 
 app.listen(3000, () => {
     console.log('server is up on port 3000')
